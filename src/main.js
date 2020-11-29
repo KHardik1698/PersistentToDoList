46e03dbda7493c4b594f463439d2d57e4f2280d5
KHardik1698 <hardik.kundalia@contentstack.com>
2020-11-29 23:43:46 +0530

    Created main.js which calls functions from other modules
    main.js calls viewLocalStorage & formSubmission

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { formSubmission } from "./helper/formSubmission.js";
+import { viewLocalStorage } from "./components/localStorage.js";
 
 window.onload = () => {
+  viewLocalStorage();
   document.forms.userInput.addEventListener("submit", formSubmission);
 };
