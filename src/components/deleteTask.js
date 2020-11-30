9302ea017bfb7f60df5d6453c3045fda5e4c1b2c
KHardik1698 <hardik.kundalia@contentstack.com>
2020-11-30 15:43:45 +0530

    Created deleteTask.js to delete the data from Local Storage
    This will be called inside the Event Listener of the Delete Button

---
+++ src/components/deleteTask.js
@@ -0,0 +1,21 @@
+import { localArr } from "./localStorage.js";
+
+export const deleteTaskElement = (id) => {
+  //   console.log("Getting the ID", findById(id));
+  localArr.splice(findById(id), 1);
+  localStorage.setItem("toDoListArr", JSON.stringify(localArr));
+  document.getElementById(id).remove();
+};
+
+let index;
+
+export const findById = (id) => {
+  // console.log("Local Array", localArr);
+  localArr.filter((task) => {
+    if (task.taskId == id) {
+      //   console.log(localArr.indexOf(task));
+      index = localArr.indexOf(task);
+    }
+  });
+  return index;
+};
