7844fd3defcff88434720cb2ce698492282e1886
KHardik1698 <hardik.kundalia@contentstack.com>
2020-11-30 15:36:47 +0530

    Created localStorage.js for storing & viewing the objects which are added to toDoList Array
    We will be able to store & display the data on the Webpage using Local Storage

---
+++ src/components/localStorage.js
@@ -0,0 +1,47 @@
+import { toDoList } from "../helper/toDoListData.js";
+import { deleteTaskElement } from "./deleteTask.js";
+import { updateStatus } from "./updateStatus.js";
+
+export let localArr = [];
+
+export const addLocalStorage = (taskObject) => {
+  localStorage.setItem("toDoListArr", JSON.stringify(localArr));
+};
+
+export const viewLocalStorage = (taskObject) => {
+  let list = JSON.parse(localStorage.getItem("toDoListArr"));
+  // console.log(list);
+  list.forEach((list) => {
+    localArr.push(list);
+  });
+  addList(list);
+  return list;
+};
+export const addList = (list) => {
+  const ul = document.getElementById("userList");
+  list.forEach((list) => {
+    const li = document.createElement("li");
+    // console.log(list);
+    li.id = list.taskId;
+    li.innerHTML = list.description;
+    li.classList.add(list.class);
+    const completeTask = document.createElement("button");
+    if (list.class == "no-strike") completeTask.innerHTML = "Complete";
+    else if (list.class == "strike") completeTask.innerHTML = "Incomplete";
+    completeTask.id = "completeTask";
+    const deleteTask = document.createElement("button");
+    deleteTask.innerHTML = "Delete";
+    deleteTask.id = "deleteTask";
+    completeTask.addEventListener("click", () => {
+      updateStatus(list, li, completeTask);
+    });
+    deleteTask.addEventListener("click", () => {
+      deleteTaskElement(li.id);
+    });
+    ul.appendChild(li);
+    li.appendChild(completeTask);
+    li.appendChild(deleteTask);
+    return ul;
+  });
+  //   localStorage.clear();
+};
